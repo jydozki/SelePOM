5227bd9271cded3c3a90d7f623ccece4338d2935
jydozki <31565967+jydozki@users.noreply.github.com>
2022-03-28 14:16:45 +0200

    New test script added

---
+++ TestPrjPOM/TestScript/Script2.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using TestPrjPOM.BaseClass;
+using TestPrjPOM.PageObject;
+
+namespace TestPrjPOM.TestScript
+{
+    class Script2:BaseTest
+    {
+        [Test]
+        public void TestMethod2()
+        {
+            var home = new Home(driver);
+
+            var searchpage= new Search(driver);
+
+            //searchpage.NavigateToResult();
+            home.NavigateToSearch();
+
+            Thread.Sleep(5000);
+            searchpage.NavigateToResult();
+
+            Thread.Sleep(5000);
+
+
+
+
+
+        }
+    }
+}
